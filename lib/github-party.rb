5be931ebbd8d7c0e8792e105a01639c3cfba912b
Josh Nichols <josh@technicalpickles.com>
2008-10-24 18:00:35 -0400

    Added search.

--- lib/github-party.rb
+++ lib/github-party.rb
@@ -1,10 +1,19 @@
 require 'rubygems'
 require 'httparty'
+require 'cgi'
 
 class GitHubParty
   include HTTParty
   base_uri 'github.com/api/v1/json'
+  
   def self.user(user)
     get("/#{user}").to_struct.user
+  end
+  
+  def self.search(terms)
+    escaped_terms = CGI.escape(terms)
+    
+    repository_hashes = get("/search/#{escaped_terms}")['repositories']
+    repository_hashes.collect {|r| r.to_struct}
   end
 end
