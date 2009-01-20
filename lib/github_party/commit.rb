241f33a0e934c39b94771c252cf028a59041d601
Josh Nichols <josh@technicalpickles.com>
2009-01-20 00:58:15 -0500

    Changed references from id to hash

--- lib/github_party/commit.rb
+++ lib/github_party/commit.rb
@@ -17,17 +17,24 @@ module GitHub
     # repository:
     #   Name of repository, required, +String+
     #
-    # id or :all
+    # hash_or_:all
+    #   a commit's hash to get a single commit
+    #   OR
+    #   :all to get the first 30 commits
     #
+    #
+    #
+    # Example:
     #
     #   GitHub::Commit.find("caged", "gitnub", :all)
     #   GitHub::Commit.find "defunkt", "github-gem", "c26d4ce9807ecf57d3f9eefe19ae64e75bcaaa8b"
-    def self.find(username, repository, id_or_all)
-      case id_or_all
+    #
+    def self.find(username, repository, hash_or_all)
+      case hash_or_all
       when :all
         self.find_all username, repository
       else
-        self.find_by_id username, repository, id_or_all
+        self.find_by_hash username, repository, hash_or_all
       end
     end
 
@@ -38,8 +45,8 @@ module GitHub
       parse(xml)
     end
 
-    def self.find_by_id(username, repository, id)
-      xml = RestClient.get("http://github.com/api/v1/xml/#{username}/#{repository}/commit/#{id}")
+    def self.find_by_hash(username, repository, hash)
+      xml = RestClient.get("http://github.com/api/v1/xml/#{username}/#{repository}/commit/#{hash}")
       commits = parse(xml)
       commits.first # TODO assert size of commits
     end
